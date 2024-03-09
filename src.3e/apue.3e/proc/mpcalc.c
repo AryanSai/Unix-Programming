8bb4d409e64c5f05e02b81e9b6c3799de2f52613
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-03-09 21:00:38 +0530

    mp calc

--- src.3e/apue.3e/proc/mpcalc.c
+++ src.3e/apue.3e/proc/mpcalc.c
@@ -1,20 +1,16 @@
 #include "apue.h"
 #include <math.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 
 /*gcc -ansi -I../include -Wall -DLINUX -D_GNU_SOURCE  mpcalc.c -o mpcalc -L../lib -lapue -lm*/
-int option = 1;
-
 void compare(int a, int b, int c, int d)
 {
     int result1, result2;
     result1 = pow(a, b);
     result2 = pow(c, d);
-    printf("------I will do the number crunching while you talk to my parent!------\n");
-    sleep(10);
+    printf("------Calculating------\n");
+    sleep(20);
     if (result1 > result2)
         printf("\n%d^%d is greater than %d^%d by pid=%ld\n", a, b, c, d, (long)getpid());
     else if (result1 < result2)
@@ -25,6 +21,7 @@ void compare(int a, int b, int c, int d)
 
 void prompt()
 {
+    int option = 1;
     pid_t pid;
     printf("\n1.Calculate\n2.Quit \nEnter your option:");
     scanf("%d", &option);
@@ -38,7 +35,6 @@ void prompt()
         else if (pid == 0) /*child*/
         {
             compare(a, b, c, d);
-            exit(0);
         }
         else
         {
