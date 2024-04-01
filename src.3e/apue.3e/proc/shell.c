55b8ac2667ee7df80bbd5856596a95e710ce06f4
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-04-01 16:15:40 +0530

    shell

--- src.3e/apue.3e/proc/shell.c
+++ src.3e/apue.3e/proc/shell.c
@@ -7,7 +7,8 @@ termination of the program.
 4. Optional: You should allow user to create / modify environment variables and export them. You should
  test whether a newly created shell variable which is exported is getting specified in the environment
  variables of the programs executed from the shell.
-5. Any additional features from your side are very welcome*/
+5. Any additional features from your side are very welcome
+*/
 
 #include "apue.h"
 #include <sys/wait.h>
@@ -15,6 +16,7 @@ termination of the program.
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 #define HISTORY_SIZE 100
 
@@ -23,10 +25,13 @@ int history_count = 0;
 
 void add_to_history(const char *command)
 {
-    if (history_count < HISTORY_SIZE) {
+    if (history_count < HISTORY_SIZE)
+    {
         strcpy(history[history_count], command);
         history_count++;
-    } else {
+    }
+    else
+    {
         printf("History is full!\n");
     }
 }
@@ -35,7 +40,8 @@ void display_history()
 {
     printf("Command History:\n");
     int i = 0;
-    while (i < history_count) {
+    while (i < history_count)
+    {
         printf("%d  %s\n", i + 1, history[i]);
         i++;
     }
@@ -47,14 +53,14 @@ void control_c(int signum)
     exit(0);
 }
 
-
 int main(void)
 {
     char buf[MAXLINE];
     int status;
 
-    signal(SIGINT, control_c);  /*signal to handle ctrl + c */
+    signal(SIGINT, control_c); /*signal to handle ctrl + c */
 
+    printf("------------Welcome to the SeaShell------------\n");
     printf("$ "); /* print dollar prompt */
     while (fgets(buf, MAXLINE, stdin) != NULL)
     {
@@ -62,9 +68,42 @@ int main(void)
             buf[strlen(buf) - 1] = 0; /* replace newline with null */
 
         add_to_history(buf);
-        
-        /*when the command is history*/
-        if (strcmp(buf, "history") == 0)
+
+        /* extract the command */
+        char *command = strtok(buf, " ");
+
+        /* when the command is cd */
+        if (strcmp(command, "cd") == 0)
+        {
+            char *arg = strtok(NULL, " ");
+            if (arg == NULL)
+            {
+                fprintf(stderr, "cd: missing argument\n");
+            }
+            else
+            {
+                if (chdir(arg) != 0) /*if cd fails*/
+                {
+                    perror("cd");
+                }
+                else
+                {
+                    char cwd[MAXLINE];                    /*to hold pwd*/
+                    if (getcwd(cwd, sizeof(cwd)) == NULL) /*get the pwd*/
+                    {
+                        perror("getcwd");
+                    }
+                    else
+                    {                                   /*set the environment variable PWD to the new one*/
+                        if (setenv("PWD", cwd, 1) != 0) /* 1 => rewrite*/
+                        {
+                            perror("setenv");
+                        }
+                    }
+                }
+            }
+        }
+        else if (strcmp(command, "history") == 0) /*when the command is history*/
         {
             display_history();
         }
